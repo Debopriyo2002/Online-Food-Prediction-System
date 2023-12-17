1e48e7ee793a078d71f9174deffea44ddd122096
Debopriyo Paul Chowdhury <debopriyochowdhury2002@gmail.com>
2023-12-17 17:25:32 +0530

    dataset changed

--- foodprediction.py
+++ foodprediction.py
@@ -5,9 +5,9 @@ import plotly.express as px
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 sns.set_theme(style="whitegrid")
-url = 'https://raw.githubusercontent.com/amankharwal/Website-data/master/onlinefoods.csv'
-data = pd.read_csv(url)
+data = pd.read_csv("onlinefoods.csv")
 print(data.head())
 
 # the online food order decisions based on the age of the customer:
@@ -30,26 +30,33 @@ print(buying_again_data.head())
 gender = buying_again_data["Gender"].value_counts()
 label = gender.index
 counts = gender.values
-colors = ['gold','lightgreen']
+colors = ["gold", "lightgreen"]
 
 fig = go.Figure(data=[go.Pie(labels=label, values=counts)])
-fig.update_layout(title_text='Who Orders Food Online More: Male Vs. Female')
-fig.update_traces(hoverinfo='label+percent', textinfo='value', textfont_size=30,
-                  marker=dict(colors=colors, line=dict(color='black', width=3)))
+fig.update_layout(title_text="Who Orders Food Online More: Male Vs. Female")
+fig.update_traces(
+    hoverinfo="label+percent",
+    textinfo="value",
+    textfont_size=30,
+    marker=dict(colors=colors, line=dict(color="black", width=3)),
+)
 fig.show()
-
 
 
 # the marital status of the customers who ordered again:
 marital = buying_again_data["Marital Status"].value_counts()
 label = marital.index
 counts = marital.values
-colors = ['gold','lightgreen']
+colors = ["gold", "lightgreen"]
 
 fig = go.Figure(data=[go.Pie(labels=label, values=counts)])
-fig.update_layout(title_text='Who Orders Food Online More: Married Vs. Singles')
-fig.update_traces(hoverinfo='label+percent', textinfo='value', textfont_size=30,
-                  marker=dict(colors=colors, line=dict(color='black', width=3)))
+fig.update_layout(title_text="Who Orders Food Online More: Married Vs. Singles")
+fig.update_traces(
+    hoverinfo="label+percent",
+    textinfo="value",
+    textfont_size=30,
+    marker=dict(colors=colors, line=dict(color="black", width=3)),
+)
 fig.show()
 
 # let’s have a look at what’s the income group of the customers who ordered the food again
@@ -57,33 +64,39 @@ fig.show()
 income = buying_again_data["Monthly Income"].value_counts()
 label = income.index
 counts = income.values
-colors = ['gold','lightgreen']
+colors = ["gold", "lightgreen"]
 
 fig = go.Figure(data=[go.Pie(labels=label, values=counts)])
-fig.update_layout(title_text='Which Income Group Orders Food Online More')
-fig.update_traces(hoverinfo='label+percent', textinfo='value', textfont_size=30,
-                  marker=dict(colors=colors, line=dict(color='black', width=3)))
+fig.update_layout(title_text="Which Income Group Orders Food Online More")
+fig.update_traces(
+    hoverinfo="label+percent",
+    textinfo="value",
+    textfont_size=30,
+    marker=dict(colors=colors, line=dict(color="black", width=3)),
+)
 fig.show()
 
 # Now let’s prepare the data for the task of training a machine learning model. Here I will convert all the categorical features into numerical values:
 
 data["Gender"] = data["Gender"].map({"Male": 1, "Female": 0})
-data["Marital Status"] = data["Marital Status"].map({"Married": 2, 
-                                                     "Single": 1, 
-                                                     "Prefer not to say": 0})
-data["Occupation"] = data["Occupation"].map({"Student": 1, 
-                                             "Employee": 2, 
-                                             "Self Employeed": 3, 
-                                             "House wife": 4})
-data["Educational Qualifications"] = data["Educational Qualifications"].map({"Graduate": 1, 
-                                                                             "Post Graduate": 2, 
-                                                                             "Ph.D": 3, "School": 4, 
-                                                                             "Uneducated": 5})
-data["Monthly Income"] = data["Monthly Income"].map({"No Income": 0, 
-                                                     "25001 to 50000": 5000, 
-                                                     "More than 50000": 7000, 
-                                                     "10001 to 25000": 25000, 
-                                                     "Below Rs.10000": 10000})
+data["Marital Status"] = data["Marital Status"].map(
+    {"Married": 2, "Single": 1, "Prefer not to say": 0}
+)
+data["Occupation"] = data["Occupation"].map(
+    {"Student": 1, "Employee": 2, "Self Employeed": 3, "House wife": 4}
+)
+data["Educational Qualifications"] = data["Educational Qualifications"].map(
+    {"Graduate": 1, "Post Graduate": 2, "Ph.D": 3, "School": 4, "Uneducated": 5}
+)
+data["Monthly Income"] = data["Monthly Income"].map(
+    {
+        "No Income": 0,
+        "25001 to 50000": 5000,
+        "More than 50000": 7000,
+        "10001 to 25000": 25000,
+        "Below Rs.10000": 10000,
+    }
+)
 data["Feedback"] = data["Feedback"].map({"Positive": 1, "Negative ": 0})
 print(data.head())
 
@@ -91,19 +104,31 @@ print(data.head())
 # Online Food Order Prediction Model
 # a machine learning model to predict whether a customer will order again or not. I will start by splitting the data into training and test sets:
 
-#splitting data
+# splitting data
 from sklearn.model_selection import train_test_split
-x = np.array(data[["Age", "Gender", "Marital Status", "Occupation", 
-                   "Monthly Income", "Educational Qualifications", 
-                   "Family size", "Pin code", "Feedback"]])
+
+x = np.array(
+    data[
+        [
+            "Age",
+            "Gender",
+            "Marital Status",
+            "Occupation",
+            "Monthly Income",
+            "Educational Qualifications",
+            "Family size",
+            "Pin code",
+            "Feedback",
+        ]
+    ]
+)
 y = np.array(data[["Output"]])
 
 
 # training a machine learning model
 from sklearn.ensemble import RandomForestClassifier
-xtrain, xtest, ytrain, ytest = train_test_split(x, y, 
-                                                test_size=0.10, 
-                                                random_state=42)
+
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.10, random_state=42)
 model = RandomForestClassifier()
 model.fit(xtrain, ytrain)
 print(model.score(xtest, ytest))
@@ -113,13 +138,24 @@ print(model.score(xtest, ytest))
 print("Enter Customer Details to Predict If the Customer Will Order Again")
 a = int(input("Enter the Age of the Customer: "))
 b = int(input("Enter the Gender of the Customer (1 = Male, 0 = Female): "))
-c = int(input("Marital Status of the Customer (1 = Single, 2 = Married, 3 = Not Revealed): "))
-d = int(input("Occupation of the Customer (Student = 1, Employee = 2, Self Employeed = 3, House wife = 4): "))
+c = int(
+    input(
+        "Marital Status of the Customer (1 = Single, 2 = Married, 3 = Not Revealed): "
+    )
+)
+d = int(
+    input(
+        "Occupation of the Customer (Student = 1, Employee = 2, Self Employeed = 3, House wife = 4): "
+    )
+)
 e = int(input("Monthly Income: "))
-f = int(input("Educational Qualification (Graduate = 1, Post Graduate = 2, Ph.D = 3, School = 4, Uneducated = 5): "))
+f = int(
+    input(
+        "Educational Qualification (Graduate = 1, Post Graduate = 2, Ph.D = 3, School = 4, Uneducated = 5): "
+    )
+)
 g = int(input("Family Size: "))
 h = int(input("Pin Code: "))
 i = int(input("Review of the Last Order (1 = Positive, 0 = Negative): "))
 features = np.array([[a, b, c, d, e, f, g, h, i]])
 print("Finding if the customer will order again: ", model.predict(features))
-
